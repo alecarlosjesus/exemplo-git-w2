315e40e84c8b228ec9c08e8cb75d6c0243f06915
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-03-07 20:49:19 -0300

    exemplo de saidas no console.

--- script.js
+++ script.js
@@ -4,3 +4,8 @@
 *Este é um
 *Comentário de bloco em Javascript
 */
+
+//Amostra de saida do console com ponteiros de marcação.
+console.log("Olá mundo!")
+console.error("Deu Erro")
+console.warn("Atenção")
